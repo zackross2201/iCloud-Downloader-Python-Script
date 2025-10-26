82ac461ff6d29795c9cbca4b3d427931dd22c917
zackross2201 <rosszr1569@gmail.com>
2025-10-26 14:58:42 +0000

    Added .bat file (Windows Only)

---
+++ launch_downloader.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting iCloud Photo Downloader...
+cd "C:\Users\zacka\OneDrive\Documents\Python Scripts\iCloud Photo Downloader"
+python icloud_downloader_ui.py
+echo.
+echo Script finished.
+pause
